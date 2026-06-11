ae474cd85e7895c8b69b4f9cbe81400d259fb484
rs1270 <rohitshrivastava0601@gmail.com>
2026-06-11 22:07:15 +0530

    large dummy dataset

--- backend/dummy_data/phonedata.js
+++ backend/dummy_data/phonedata.js
@@ -11,6 +11,17 @@ const phones = [
     rating: 4.6
   },
   {
+    name: "Pixel 9a",
+    brand: "Google",
+    price: 49999,
+    camera: "48MP OIS",
+    battery: "5100mAh",
+    charging: "23W",
+    display: "6.3 OLED",
+    processor: "Tensor G4",
+    rating: 4.7
+  },
+  {
     name: "OnePlus 12R",
     brand: "OnePlus",
     price: 39999,
@@ -22,6 +33,28 @@ const phones = [
     rating: 4.7
   },
   {
+    name: "OnePlus Nord 4",
+    brand: "OnePlus",
+    price: 28999,
+    camera: "50MP OIS",
+    battery: "5500mAh",
+    charging: "100W",
+    display: "6.74 AMOLED",
+    processor: "Snapdragon 7+ Gen 3",
+    rating: 4.5
+  },
+  {
+    name: "OnePlus Nord CE4",
+    brand: "OnePlus",
+    price: 24999,
+    camera: "50MP OIS",
+    battery: "5500mAh",
+    charging: "100W",
+    display: "6.7 AMOLED",
+    processor: "Snapdragon 7 Gen 3",
+    rating: 4.4
+  },
+  {
     name: "Samsung Galaxy A56",
     brand: "Samsung",
     price: 27999,
@@ -29,9 +62,164 @@ const phones = [
     battery: "5000mAh",
     charging: "45W",
     display: "6.7 AMOLED",
-    processor: "Exynos",
+    processor: "Exynos 1580",
     rating: 4.4
+  },
+  {
+    name: "Samsung Galaxy A36",
+    brand: "Samsung",
+    price: 22999,
+    camera: "50MP OIS",
+    battery: "5000mAh",
+    charging: "45W",
+    display: "6.7 AMOLED",
+    processor: "Snapdragon 6 Gen 3",
+    rating: 4.3
+  },
+  {
+    name: "Samsung Galaxy S24 FE",
+    brand: "Samsung",
+    price: 54999,
+    camera: "50MP OIS",
+    battery: "4700mAh",
+    charging: "25W",
+    display: "6.7 Dynamic AMOLED",
+    processor: "Exynos 2400e",
+    rating: 4.7
+  },
+  {
+    name: "iPhone 15",
+    brand: "Apple",
+    price: 64999,
+    camera: "48MP",
+    battery: "3349mAh",
+    charging: "20W",
+    display: "6.1 OLED",
+    processor: "A16 Bionic",
+    rating: 4.8
+  },
+  {
+    name: "iPhone 16",
+    brand: "Apple",
+    price: 79999,
+    camera: "48MP",
+    battery: "3561mAh",
+    charging: "25W",
+    display: "6.1 OLED",
+    processor: "A18",
+    rating: 4.9
+  },
+  {
+    name: "iPhone 16 Plus",
+    brand: "Apple",
+    price: 89999,
+    camera: "48MP",
+    battery: "4674mAh",
+    charging: "25W",
+    display: "6.7 OLED",
+    processor: "A18",
+    rating: 4.9
+  },
+  {
+    name: "Nothing Phone 3a",
+    brand: "Nothing",
+    price: 27999,
+    camera: "50MP OIS",
+    battery: "5000mAh",
+    charging: "45W",
+    display: "6.77 AMOLED",
+    processor: "Snapdragon 7s Gen 3",
+    rating: 4.4
+  },
+  {
+    name: "Nothing Phone 3a Pro",
+    brand: "Nothing",
+    price: 32999,
+    camera: "50MP OIS",
+    battery: "5000mAh",
+    charging: "50W",
+    display: "6.77 AMOLED",
+    processor: "Snapdragon 7s Gen 3",
+    rating: 4.5
+  },
+  {
+    name: "Redmi Note 14 Pro",
+    brand: "Xiaomi",
+    price: 25999,
+    camera: "200MP OIS",
+    battery: "5500mAh",
+    charging: "90W",
+    display: "6.67 AMOLED",
+    processor: "Dimensity 7300",
+    rating: 4.4
+  },
+  {
+    name: "Redmi 13 5G",
+    brand: "Xiaomi",
+    price: 13999,
+    camera: "108MP",
+    battery: "5030mAh",
+    charging: "33W",
+    display: "6.79 LCD",
+    processor: "Snapdragon 4 Gen 2",
+    rating: 4.2
+  },
+  {
+    name: "Xiaomi 14 Civi",
+    brand: "Xiaomi",
+    price: 42999,
+    camera: "50MP Leica",
+    battery: "4700mAh",
+    charging: "67W",
+    display: "6.55 AMOLED",
+    processor: "Snapdragon 8s Gen 3",
+    rating: 4.6
+  },
+  {
+    name: "Realme GT 7",
+    brand: "Realme",
+    price: 39999,
+    camera: "50MP OIS",
+    battery: "7000mAh",
+    charging: "120W",
+    display: "6.78 AMOLED",
+    processor: "Dimensity 9400e",
+    rating: 4.7
+  },
+  {
+    name: "Realme Narzo 80 Pro",
+    brand: "Realme",
+    price: 19999,
+    camera: "50MP",
+    battery: "6000mAh",
+    charging: "80W",
+    display: "6.72 AMOLED",
+    processor: "Dimensity 7400",
+    rating: 4.3
+  },
+  {
+    name: "Vivo T4",
+    brand: "Vivo",
+    price: 21999,
+    camera: "50MP OIS",
+    battery: "7300mAh",
+    charging: "90W",
+    display: "6.77 AMOLED",
+    processor: "Snapdragon 7s Gen 3",
+    rating: 4.5
+  },
+  {
+    name: "Vivo V50",
+    brand: "Vivo",
+    price: 34999,
+    camera: "50MP ZEISS",
+    battery: "6000mAh",
+    charging: "90W",
+    display: "6.77 AMOLED",
+    processor: "Snapdragon 7 Gen 3",
+    rating: 4.5
   }
 ];
 
 module.exports = phones;
+
