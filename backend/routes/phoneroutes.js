8f05301c80470bdf9d812390202be96c8cbaaa37
rs1270 <rohitshrivastava0601@gmail.com>
2026-06-12 22:32:24 +0530

    added gemini ai

--- backend/routes/phoneroutes.js
+++ backend/routes/phoneroutes.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const {getAllPhones, searchPhones}=require('../controllers/phonecontroller');
 router.get('/search', searchPhones);
 router.get('/', getAllPhones);
+router.get('/gemini-test', async (req, res) => {
+  const askGemini = require('../services/gemini');
 
+  const result = await askGemini(
+    'Best camera phone under 30000'
+  );
+
+  res.json({ result });
+});
 
 module.exports = router;
